47862730b6638b6d2bc0e9a5a04884a17a77c0a4
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-03-10 13:39:34 +0100

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Splitter from "./components/Splitter";
 
 function App() {
 	return (
-		<div className="content-center justify-items-center min-h-screen bg-cyan-400">
+		<div className="content-center justify-items-center min-h-screen py-40 bg-cyan-400">
 			<img src={Logo} alt="Logo" className="w-full max-w-[86.66px] mb-[88px]" />
 			<Splitter />
 		</div>
