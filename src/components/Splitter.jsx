773f0a892dc5a9cfe26d41406c592af0a54160c2
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-03-10 11:41:29 +0100

    made it responsive

--- src/components/Splitter.jsx
+++ src/components/Splitter.jsx
@@ -29,7 +29,6 @@ export default function Splitter() {
 			setBill(value);
 		} else if (id === "numberOfPeople") {
 			setNumberOfPeople(value);
-
 			if (value === "" || +value > 0) {
 				setError("");
 			} else if (+value === 0) {
@@ -70,9 +69,9 @@ export default function Splitter() {
 	};
 
 	return (
-		<main className="flex max-md:flex-col gap-[48px] max-md:w-max justify-between w-full max-w-[920px] bg-white px-10 py-8 rounded-3xl">
+		<main className="flex max-md:flex-col gap-[3rem] max-md:w-max justify-between w-full max-w-[57.5rem] bg-white px-[2rem] py-[2rem] rounded-[1.5rem]">
 			{/* Input Section */}
-			<section className="w-full max-w-[380px] flex flex-col gap-12">
+			<section className="w-full max-w-[23.75rem] flex flex-col gap-[3rem]">
 				<TextInput
 					icon={Dollar}
 					label="Bill"
@@ -85,8 +84,8 @@ export default function Splitter() {
 				/>
 
 				<fieldset>
-					<legend className="font-bold text-cyan-600 text-[16px] mb-2">Select Tip %</legend>
-					<div className="grid grid-cols-2 lg:grid-cols-3 gap-y-[14px] place-items-center gap-x-[13px] w-full">
+					<legend className="font-bold text-cyan-600 text-[1rem] mb-[0.5rem]">Select Tip %</legend>
+					<div className="grid grid-cols-2 lg:grid-cols-3 gap-y-[0.875rem] place-items-center gap-x-[0.8125rem] w-full">
 						<TipButton
 							tip="5%"
 							handleButtonClick={handleButtonClick}
@@ -146,9 +145,9 @@ export default function Splitter() {
 			</section>
 
 			{/* Results Section */}
-			<aside className="w-full min-h-full max-w-[413px] bg-cyan-800 rounded-2xl p-10">
-				<div className="flex flex-col h-full">
-					<div className="flex flex-col gap-12 mb-auto">
+			<aside className="w-full min-h-full max-w-[25.8125rem] bg-cyan-800 rounded-[1rem] p-[2.5rem] max-md:p-6">
+				<div className="flex flex-col h-full max-md:gap-10">
+					<div className="flex flex-col gap-[3rem] mb-auto">
 						<OutputDisplay label="Tip Amount" subLabel="/person" value={tipAmount} />
 						<OutputDisplay label="Total" subLabel="/person" value={total} />
 					</div>
@@ -177,23 +176,25 @@ function TextInput({
 		<div className="w-full">
 			<label
 				htmlFor={id}
-				className={`font-bold text-[16px] text-cyan-600 ${
+				className={`font-bold text-[1rem] text-cyan-600 ${
 					error ? "flex justify-between items-center" : ""
 				}`}
 			>
-				<p> {label}</p>
+				<p>{label}</p>
 				{error ? <p className="text-red-400">{error}</p> : ""}
 			</label>
 			<div
-				className={`flex justify-between items-center rounded-[6px] w-full px-4 py-3.5 h-[48px]  bg-cyan-300 ${
-					!error ? "hover:border-[2px] hover:border-[#26C0AB]" : "border-[2px] border-red-400"
+				className={`flex justify-between items-center rounded-[0.375rem] w-full px-[1rem] py-[0.875rem] h-[3rem] bg-cyan-300 ${
+					!error && !window.matchMedia("(hover: none)").matches
+						? "hover:border-[0.125rem] hover:border-[#26C0AB]"
+						: "border-[0.125rem] border-red-400"
 				}`}
 				style={{
-					border: focusedInput === id && !error ? "2px #26C0AB solid" : "",
-					marginBlockStart: id === "customInput" ? "" : "8px",
+					border: focusedInput === id && !error ? "0.125rem #26C0AB solid" : "",
+					marginBlockStart: id === "customInput" ? "" : "0.5rem",
 				}}
 			>
-				<img src={icon} alt={label} className="max-h-[16px]" />
+				<img src={icon} alt={label} className="max-h-[1rem]" />
 				<input
 					type="number"
 					id={id}
@@ -226,13 +227,13 @@ function TipButton({ tip, custom = false, isActive, handleButtonClick }) {
 	} else if (isActive) {
 		backgroundColour = "bg-cyan-700 text-cyan-800";
 	} else {
-		backgroundColour = "text-white bg-[hsl(183,100%,15%)]";
+		backgroundColour = "text-white bg-cyan-800";
 	}
 
 	return (
 		<button
 			type="button"
-			className={`h-[48px] w-full rounded-[4px] font-bold max-w-[118px] cursor-pointer ${backgroundColour} ${
+			className={`h-[3rem] w-full rounded-[0.25rem] font-bold  cursor-pointer ${backgroundColour} ${
 				!custom && !isActive ? "hover:bg-[#9EE9DF] hover:text-[#00494D]" : ""
 			}`}
 			onClick={() => {
@@ -247,21 +248,21 @@ function TipButton({ tip, custom = false, isActive, handleButtonClick }) {
 function OutputDisplay({ label, subLabel, value }) {
 	return (
 		<div className="flex justify-between items-center">
-			<div className="flex flex-col gap-[8px]">
-				<h3 className="text-cyan-300 text-[16px] font-bold">{label}</h3>
-				<p className="text-cyan-500 text-[14px]">{subLabel}</p>
+			<div className="flex flex-col gap-[0.5rem]">
+				<h3 className="text-cyan-300 text-[1rem] font-bold max-md:text-[0.8rem]">{label}</h3>
+				<p className="text-cyan-500 text-[0.875rem] max-md:text-[0.6rem]">{subLabel}</p>
 			</div>
-			<output className="text-cyan-700 font-bold text-5xl">${value}</output>
+			<output className="text-cyan-700 font-bold text-[3rem] max-md:text-[2.5rem]">${value}</output>
 		</div>
 	);
 }
 
-function ResetButton({ bill, handleReset, tipValue }) {
+function ResetButton({ bill, handleReset }) {
 	return (
 		<button
 			type="reset"
 			disabled={bill.trim() > 0 ? false : true}
-			className="h-[48px] rounded-[4px] font-bold w-full text-[hsl(183,100%,15%)] bg-[hsl(172,67%,45%)] disabled:opacity-50"
+			className="h-[3rem] rounded-[0.25rem] font-bold w-full text-[hsl(183,100%,15%)] bg-[hsl(172,67%,45%)] disabled:opacity-50"
 			style={{ cursor: bill.trim() > 0 ? "pointer" : "" }}
 			onClick={handleReset}
 		>
